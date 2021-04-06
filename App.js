1c4fb3eec5d64739e35974d2145f99c159c8c248
hedbergare <hedbergjacob@gmail.com>
2021-04-06 10:22:45 +0200

    Firebase config initialized

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import ApiKeys from "./constants/ApiKeys";
+import * as firebase from "firebase";
 
 export default function App() {
+  //Initializing firebase
+  if (!firebase.apps.length) {
+    firebase.initializeApp(ApiKeys.FirebaseConfig);
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -14,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "yellow",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
